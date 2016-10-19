f82e02473d940c205357ff87a115eb9e30823e34
pavel <pavel.kukushkin@brightcomputing.com>
2016-10-19 11:21:45 +0200

    Correct azure version in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ setup(
     version='0.1',
     description='Bright CoD image Azure integration',
     packages=['cmazure'],
-    install_requires=["azure"],
+    install_requires=[
+        "msrest<0.5.0,>=0.4.0",
+        "azure==2.0.0rc6"
+    ],
     entry_points={
         "console_scripts":
         "cmazure=cmazure.cli:main"
